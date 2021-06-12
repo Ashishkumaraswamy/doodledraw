07c75695a08469bdd161f4bb6828eb533041ee49
Ashish <ashishkumaraswamy@gmail.com>
2021-06-12 18:39:52 +0530

    commit home page start

--- doodledraw/urls.py
+++ doodledraw/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf import settings
 from django.views.static import serve
@@ -22,7 +22,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path("", include("drawapp.urls")),
     url(r'^media/(?P<path>.*)$', serve,
         {'document_root':       settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,
