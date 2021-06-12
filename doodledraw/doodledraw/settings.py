fab3265d03d5e2e1ff3cc3d62f0996b344a4a547
Ashish <ashishkumaraswamy@gmail.com>
2021-06-12 17:10:55 +0530

    commit for deployment

--- doodledraw/doodledraw/settings.py
+++ doodledraw/doodledraw/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import django_heroku
 from pathlib import Path
 import os
-import whitenoise
+# import whitenoise
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-kb08zaxc2z9td2@-ilbt3^ky6dc0**+ao(lyvtlper+l$4=t3!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['doodledrawarmy.herokuapp.com', 'http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ['http://127.0.0.1:8000/']
 
 
 # Application definition
--- doodledraw/doodledraw/urls.py
+++ doodledraw/doodledraw/urls.py
@@ -16,6 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
