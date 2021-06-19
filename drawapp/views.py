585ffd77dc790849790ee4d479f3bb3b2ebf610e
Ashish <ashishkumaraswamy@gmail.com>
2021-06-19 19:25:12 +0530

    commit added 4 neural network models

--- drawapp/views.py
+++ drawapp/views.py
@@ -8,70 +8,38 @@ import io
 import matplotlib.pyplot as plt
 import numpy as np
 from drawapp.classify import classify
+from PIL import Image, ImageOps
+
 
 # Create your views here.
 
+imgquescat = []
 imgques = []
 cnt = -1
-gamecat = ['airplane', 'alarm clock', 'angel', 'ant', 'apple', 'arm',
-           'armchair', 'ashtray', 'axe', 'backpack', 'banana', 'barn',
-           'baseball bat', 'basket', 'bathtub', 'bear ', 'bed', 'bee',
-           'beer-mug', 'bell', 'bench', 'bicycle', 'binoculars', 'blimp',
-           'book', 'bookshelf', 'boomerang', 'bottle opener', 'bowl', 'brain',
-           'bread', 'bridge', 'bulldozer', 'bus', 'bush', 'butterfly',
-           'cabinet', 'cactus', 'cake', 'calculator', 'camel', 'camera',
-           'candle', 'cannon', 'canoe', 'car ', 'carrot', 'castle', 'cat',
-           'cell phone', 'chair', 'chandelier', 'church', 'cigarette',
-           'cloud', 'comb', 'computer monitor', 'computer-mouse', 'couch',
-           'cow', 'crab', 'crane ', 'crocodile', 'crown', 'cup', 'diamond',
-           'dog', 'dolphin', 'donut', 'door', 'door handle', 'dragon', 'duck',
-           'ear', 'elephant', 'envelope', 'eye', 'eyeglasses', 'face', 'fan',
-           'feather', 'fire hydrant', 'fish', 'flashlight', 'floor lamp',
-           'flower with stem', 'flying bird', 'flying saucer', 'foot', 'fork',
-           'frog', 'frying-pan', 'giraffe', 'grapes', 'grenade', 'guitar',
-           'hamburger', 'hammer', 'hand', 'harp', 'hat', 'head',
-           'head-phones', 'hedgehog', 'helicopter', 'helmet', 'horse',
-           'hot air balloon', 'hot-dog', 'hourglass', 'house',
-           'human-skeleton', 'ice-cream-cone', 'ipod', 'kangaroo', 'key',
-           'keyboard', 'knife', 'ladder', 'laptop', 'leaf', 'lightbulb',
-           'lighter', 'lion', 'lobster', 'loudspeaker', 'mailbox',
-           'megaphone', 'mermaid', 'microphone', 'microscope', 'monkey',
-           'moon', 'mosquito', 'motorbike', 'mouse ', 'mouth', 'mug',
-           'mushroom', 'nose', 'octopus', 'owl', 'palm tree', 'panda',
-           'paper clip', 'parachute', 'parking meter', 'parrot', 'pear',
-           'pen', 'penguin', 'person sitting', 'person walking', 'piano',
-           'pickup truck', 'pig', 'pigeon', 'pineapple', 'pipe ', 'pizza',
-           'potted plant', 'power outlet', 'present', 'pretzel', 'pumpkin',
-           'purse', 'rabbit', 'race car', 'radio', 'rainbow', 'revolver',
-           'rifle', 'rollerblades', 'rooster', 'sailboat', 'santa claus',
-           'satellite', 'satellite dish', 'saxophone', 'scissors', 'scorpion',
-           'screwdriver', 'sea turtle', 'seagull', 'shark', 'sheep', 'ship',
-           'shoe', 'shovel', 'skateboard', 'skull', 'skyscraper', 'snail',
-           'snake', 'snowboard', 'snowman', 'socks', 'space shuttle',
-           'speed-boat', 'spider', 'sponge bob', 'spoon', 'squirrel',
-           'standing bird', 'stapler', 'strawberry', 'streetlight',
-           'submarine', 'suitcase', 'sun', 'suv', 'swan', 'sword', 'syringe',
-           't-shirt', 'table', 'tablelamp', 'teacup', 'teapot', 'teddy-bear',
-           'telephone', 'tennis-racket', 'tent', 'tiger', 'tire', 'toilet',
-           'tomato', 'tooth', 'toothbrush', 'tractor', 'traffic light',
-           'train', 'tree', 'trombone', 'trousers', 'truck', 'trumpet', 'tv',
-           'umbrella', 'van', 'vase', 'violin', 'walkie talkie', 'wheel',
-           'wheelbarrow', 'windmill', 'wine-bottle', 'wineglass',
-           'wrist-watch', 'zebra']
-gamecat = gamecat[:5]
+majorcat = ['vehicles', 'utils']
+utils = ['backpack', 'screwdriver', 'bucket', 'candle', 'cup']
+sports = ['basketball', 'bat', 'drums', 'baseball bat', 'pool']
+vehicle = ['airplane', 'aircraft carrier', 'bus', 'van',
+           'truck', 'train', 'tractor', 'parachute', 'bicycle']
 
 
 def random():
-    global gamecat
+    global majorcat
     global imgques
     global cnt
+    global imgquescat
 
-    ques = np.random.choice(gamecat)
-    if ques in imgques:
+    ques = np.random.choice(majorcat)
+    if ques in imgquescat:
         random()
     else:
-        ques = ques.upper()
-        imgques.append(ques)
+        imgquescat.append(ques)
+        if ques == 'vehicles':
+            imgques.append(np.random.choice(vehicle).upper())
+        elif ques == 'sports':
+            imgques.append(np.random.choice(sports).upper())
+        elif ques == 'utils':
+            imgques.append(np.random.choice(utils).upper())
         cnt += 1
 
 
@@ -84,6 +52,7 @@ def game(request):
     global imgques
     random()
     if cnt < 6:
+        print(cnt)
         return render(request, "mainpage.html", {
             'imgques': imgques[cnt],
         })
@@ -94,6 +63,7 @@ def game(request):
 def get_canvas(request):
     global imgques
     global cnt
+
     if request.method == "POST":
         captured_image = request.POST['canvas_data']
         imgstr = re.search('base64,(.*)', captured_image).group(1)
@@ -101,8 +71,20 @@ def get_canvas(request):
         # print(imgstr)
         tempimg = io.BytesIO(imgstr)
         im = Image.open(tempimg)
+        im = ImageOps.grayscale(im)
+        im.show()
         im = np.array(im)
-        # im.show()
-        cat = classify(im, gamecat)
-        print(imgques[cnt])
-        return HttpResponse(cat)
+
+        cat = classify(im, imgquescat[cnt])
+        if imgquescat[cnt] == 'vehicles':
+            global vehicle
+            print(vehicle[cat])
+            return HttpResponse(vehicle[cat])
+        elif imgquescat[cnt] == 'sports':
+            global sports
+            print(sports[cat])
+            return HttpResponse(sports[cat])
+        elif imgquescat[cnt] == 'utils':
+            global utils
+            print(utils[cat])
+            return HttpResponse(utils[cat])
