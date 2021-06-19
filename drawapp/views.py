f0457ab0f4222013804abadd18db8318f6c9bf3d
mathanamathav <62739618+mathanamathav@users.noreply.github.com>
2021-06-19 20:51:48 +0530

    want to fix some bugs...

--- drawapp/views.py
+++ drawapp/views.py
@@ -4,24 +4,32 @@ from django.template import RequestContext
 from PIL import Image
 import re
 import base64
-import io
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 from drawapp.classify import classify
 from PIL import Image, ImageOps
+import cv2
+from tensorflow.keras.models import load_model
 
 
 # Create your views here.
-
+IMG_SIZE = 28
 imgquescat = []
 imgques = []
 cnt = -1
-majorcat = ['vehicles', 'utils']
-utils = ['backpack', 'screwdriver', 'bucket', 'candle', 'cup']
-sports = ['basketball', 'bat', 'drums', 'baseball bat', 'pool']
+majorcat = ['vehicle', 'utils','sports','shapes','art','human']
+utils =  ['backpack','screwdriver','bucket','candle','cup','belt','hat','scissors','sword','tent']
+sports = ['basketball', 'bat','drums','baseball bat','pool']
+shapes = ['hexagon','octagon','circle','line','triangle']
+art = ['pond', 'pool', 'popsicle','toilet','boomerang']
+human =  ['hand','foot','eye','face','toe','tooth']
 vehicle = ['airplane', 'aircraft carrier', 'bus', 'van',
            'truck', 'train', 'tractor', 'parachute', 'bicycle']
 
+def normalize(data):
+    "Takes a list or a list of lists and returns its normalized form"
+    return np.interp(data, [0, 255], [-1, 1])
 
 def random():
     global majorcat
@@ -68,23 +76,44 @@ def get_canvas(request):
         captured_image = request.POST['canvas_data']
         imgstr = re.search('base64,(.*)', captured_image).group(1)
         imgstr = base64.b64decode(imgstr)
+        with open('temp.png', 'wb') as output:
+            output.write(imgstr)
+        im = cv2.imread('temp.png' ,cv2.IMREAD_GRAYSCALE)
+        
         # print(imgstr)
-        tempimg = io.BytesIO(imgstr)
-        im = Image.open(tempimg)
-        im = ImageOps.grayscale(im)
-        im.show()
-        im = np.array(im)
+        # tempimg = io.BytesIO(imgstr)
+        # im = Image.open(tempimg)
+        # im = ImageOps.grayscale(im)
+        # im.show()
+        #im = np.array(imgstr)
 
-        cat = classify(im, imgquescat[cnt])
+        # cat = classify(im, imgquescat[cnt])
+        x = cv2.resize(im, (28, 28))
+        x = np.expand_dims(x, axis=0)
+        x = np.reshape(x, (28, 28, 1))
+        # invert the colors
+        x = np.invert(x)
+        # brighten the image by 60%
+        for i in range(len(x)):
+            for j in range(len(x)):
+                if x[i][j] > 50:
+                    x[i][j] = min(255, x[i][j] + x[i][j] * 0.60)
+        x = normalize(x)
+
+        model = load_model('drawapp\\'+imgquescat[cnt]+'.h5')
+        val = model.predict(np.array([x]))
         if imgquescat[cnt] == 'vehicles':
             global vehicle
-            print(vehicle[cat])
-            return HttpResponse(vehicle[cat])
+            pred = vehicle[np.argmax(val)]
+            print(pred)
+            return HttpResponse(pred)
         elif imgquescat[cnt] == 'sports':
             global sports
-            print(sports[cat])
-            return HttpResponse(sports[cat])
+            pred = sports[np.argmax(val)]
+            print(pred)
+            return HttpResponse(pred)
         elif imgquescat[cnt] == 'utils':
             global utils
-            print(utils[cat])
-            return HttpResponse(utils[cat])
+            pred = utils[np.argmax(val)]
+            print(pred)
+            return HttpResponse(pred)
